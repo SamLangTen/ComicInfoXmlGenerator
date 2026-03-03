3ae5107234b4fc63ac59af4f2cf0f5d03463e893
samlangten <samlangten@outlook.com>
2026-03-03 23:19:04 +0800

    test(scraper): Refactor tests to target LocalFilenameScraper and LlmFilenameScraper

--- test/test_filename_scraper.py
+++ test/test_filename_scraper.py
@@ -4,55 +4,37 @@ import tempfile
 import shutil
 from unittest.mock import patch, MagicMock
 from src.comic_info import ComicInfo
-from src.scraper import FilenameScraper, RegexFilenameScraper, OldSchoolFilenameScraper, LlmFilenameScraper
+from src.scraper import LocalFilenameScraper, LlmFilenameScraper
 
-class TestRegexFilenameScraper(unittest.TestCase):
-    def setUp(self):
-        self.scraper = RegexFilenameScraper()
-
-    def test_basic_filename(self):
-        comic = ComicInfo(path="Series 001.cbz")
-        result = self.scraper.search(comic)
-        self.assertEqual(result.Series, "Series")
-        self.assertEqual(result.Number, "001")
-
-    def test_complex_filename(self):
-        comic = ComicInfo(path="Amazing Spider-Man V01 #01 (2024).cbz")
-        result = self.scraper.search(comic)
-        self.assertEqual(result.Series, "Amazing Spider-Man")
-        self.assertEqual(result.Volume, 1)
-        self.assertEqual(result.Number, "01")
-        self.assertEqual(result.Year, 2024)
-
-    def test_filename_with_year_and_no_volume(self):
-        comic = ComicInfo(path="Batman (2020) #05.cbr")
-        result = self.scraper.search(comic)
-        self.assertEqual(result.Series, "Batman")
-        self.assertEqual(result.Number, "05")
-        self.assertEqual(result.Year, 2020)
-
-class TestOldSchoolFilenameScraper(unittest.TestCase):
+class TestLocalFilenameScraper(unittest.TestCase):
     def setUp(self):
         self.test_dir = tempfile.mkdtemp()
-        self.scraper = OldSchoolFilenameScraper()
+        self.scraper = LocalFilenameScraper()
 
     def tearDown(self):
         shutil.rmtree(self.test_dir)
 
-    def test_directory_inference(self):
-        files = [
-            "Amazing Spider-Man v01.cbz",
-            "Amazing Spider-Man v02.cbz",
-            "Amazing Spider-Man v03.cbz"
-        ]
-        for f in files:
-            with open(os.path.join(self.test_dir, f), 'w') as tmp:
-                tmp.write("")
-
-        comic = ComicInfo(path=os.path.join(self.test_dir, "Amazing Spider-Man v02.cbz"))
-        result = self.scraper.search(comic)
+    def test_regex_fallback(self):
+        # Single file should use regex logic
+        filename = "Amazing Spider-Man v01 (2024).cbz"
+        path = os.path.join(self.test_dir, filename)
+        with open(path, 'w') as f: f.write("")
         
+        comic = ComicInfo(path=path)
+        result = self.scraper.search(comic)
         self.assertEqual(result.Series, "Amazing Spider-Man")
+        self.assertEqual(result.Volume, 1)
+        self.assertEqual(result.Year, 2024)
+
+    def test_directory_inference(self):
+        # Multiple files should use context logic
+        files = ["One Piece 01.cbz", "One Piece 02.cbz"]
+        for f in files:
+            with open(os.path.join(self.test_dir, f), 'w') as tmp: tmp.write("")
+        
+        comic = ComicInfo(path=os.path.join(self.test_dir, "One Piece 02.cbz"))
+        result = self.scraper.search(comic)
+        self.assertEqual(result.Series, "One Piece")
         self.assertEqual(result.Number, "02")
 
 class TestLlmFilenameScraper(unittest.TestCase):
